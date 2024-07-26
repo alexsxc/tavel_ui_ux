7f0ff8d8f5bb781b4ec41971be3ff9d3935eb893
alexsxc <alexanderqwsrtuz@gmail.com>
2024-07-26 17:50:53 +0300

    buttonprops and add files tsx

---
+++ components/Hero.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Hero = () => {
+	return (
+		<section className='max-container padding-container flex flex-col gap-20 py-10 md:gap-28 lg:py20 xl:flex-row  border-2 border-red-500'>
+			<div className='hero-map' />
+		</section>
+	)
+}
+
+export default Hero
